42be9e083d81fa7e40110c774631f7f900531a31
Andrii <bezkrovnyyav@gmail.com>
2021-10-20 13:35:51 +0300

    Tasks 1-4 is made

---
+++ task1_js.js
@@ -0,0 +1,28 @@
+// 1. Write a JS program to compute and print the sum of the two given integers
+
+let a = 5;
+let b = 6;
+console.log(a + b);
+
+// 2. Write a JS program to create a new string adding “Test_" in front of a given string
+
+let someStr = `Camp`;
+let newStr = 'Test_' + someStr;
+console.log(newStr);
+
+// 3. Write a JS program to combine and print your first, last name and age
+
+const name = 'Andrii',
+      sirname = 'Bezkrovnyi',
+      age = 28;
+console.log(`Hi, my name is ${name}, my sirname is ${sirname}. I am ${age} years old.`);
+
+// 4. Write a JS program to check a given integer is bigger than 100
+
+let someNumber = 999;
+console.log(someNumber > 100);
+
+// 5.	Write a JS program to find if the first number is larger from the two given positive integers
+
+
+
